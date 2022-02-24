0a2dceb45f3f56f186693436ef1d31ff7fa44ac4
SashkaShashka <sasha151299qwe@gmail.com>
2022-02-24 20:53:36 +0400

    Добавил немного API для маленьких карточек

--- NonLisbonProject/wwwroot/weather/js/index.js
+++ NonLisbonProject/wwwroot/weather/js/index.js
@@ -1,6 +1,5 @@
 import api from "/utils/api.js";
 import { getNowTime, _getMonth } from "../../utils/time_reader.js";
-import { apiRequest } from "../../utils/api.js";
 /*
 function getIconRequest() {
     fetch("http://openweathermap.org/img/wn/10d@2x.png")
@@ -14,27 +13,114 @@ function getIconRequest() {
 const iconCard = document.querySelector("icon_weather");
 const icon = await getIconRequest();
 */
-function getIconRequest() {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=51.5085&lon=-0.1257&appid=8e60906d7c520861f76a479b2765c285&units=metric&lang=ru")
-        .then((response) => response.json())
-        .then((imageBlob) => {
-            console.log(imageBlob);
-        });
-}
-var date = new Date();
-const iconCard = document.querySelector("icon_weather");
-const icon = await getIconRequest();
-const timeNow = document.querySelector("#time_now");
-console.log(timeNow.innerHTML);
-timeNow.innerHTML = getNowTime(date);
 
-const day_one = document.querySelector("#day_one");
-const day_two = document.querySelector("#day_two");
-const day_three = document.querySelector("#day_three");
-const day_four = document.querySelector("#day_four");
-date.setDate(date.getDate() + 2);
-day_two.firstElementChild.innerHTML = date.getDate() + " " + _getMonth(date);
-date.setDate(date.getDate() + 1);
-day_three.firstElementChild.innerHTML = date.getDate() + " " + _getMonth(date);
-date.setDate(date.getDate() + 1);
-day_four.firstElementChild.innerHTML = date.getDate() + " " + _getMonth(date);
+const KeyAPI = "8e60906d7c520861f76a479b2765c285";
+var latitude = 53.2106474;
+var longitude = 50.1901868;
+let weather;
+function getWeatherRequest(lat, lon, key) {
+    return api.get("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + key + "&units=metric&lang=ru");
+}
+
+//
+var options = {
+    enableHighAccuracy: false,
+    timeout: 5000,
+    maximumAge: 0,
+};
+
+function success(pos) {
+    var crd = pos.coords;
+    latitude = crd.latitude;
+    console.log(latitude);
+    longitude = crd.longitude;
+    console.log(longitude);
+}
+
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+    console.log("Сука, дай местоположение свое!");
+}
+
+async function fillCards(mainDay) {
+    for (let index = 0; index < 5; index++) {
+        if (index == mainDay) {
+            let day = templateMainCard.content.cloneNode(true);
+            fillCard(day, index, true);
+            container.prepend(day);
+        } else {
+            let day = templateCard.content.cloneNode(true);
+            fillCard(day, index);
+            container.append(day);
+        }
+    }
+}
+function findIndexNextDay(weather) {
+    for (let i = 0; i < weather.list.length; i++) {
+        const element = weather.list[i].dt_txt;
+        if (element.includes("-" + (date.getDate() + 1))) {
+            return i;
+        }
+    }
+}
+function fillCard(card, indexDay, main = false) {
+    switch (indexDay) {
+        case 0:
+            if (main) {
+                card.querySelector("#dayName").innerHTML = getNowTime(date);
+            } else {
+                card.querySelector("#dayName").innerHTML = "Сегодня";
+            }
+            break;
+        case 1:
+            card.querySelector("#dayName").innerHTML = "Завтра";
+            date.setDate(date.getDate() + 2);
+            break;
+        default:
+            card.querySelector("#dayName").innerHTML = date.getDate() + " " + _getMonth(date);
+            date.setDate(date.getDate() + 1);
+    }
+    if (!main) {
+        intexTomorrow += 4;
+        var weatherOfDat = weather.list[intexTomorrow];
+        card.querySelector("#degrees").innerHTML = Math.round(weatherOfDat.main.temp) + "&deg";
+        card.querySelector("#icon_weather").setAttribute("src", "http://openweathermap.org/img/wn/" + weatherOfDat.weather[0].icon + "@2x.png");
+        var description = weatherOfDat.weather[0].description;
+        card.querySelector("#description").innerHTML = description[0].toUpperCase() + description.substring(1);
+        card.querySelector("#wind").innerHTML = weatherOfDat.wind.speed.toFixed(1) + " м/с, " + directionByDergees(weatherOfDat.wind.deg);
+        card.querySelector("#water").innerHTML = weatherOfDat.main.humidity + "%";
+        intexTomorrow += 4;
+    } else {
+        var weatherOfDat = weather.list[intexTomorrow];
+
+        card.querySelector("#degrees").innerHTML = Math.round(weatherOfDat.main.temp) + "&deg";
+        card.querySelector("#icon_weather").setAttribute("src", "http://openweathermap.org/img/wn/" + weatherOfDat.weather[0].icon + "@2x.png");
+        var description = weatherOfDat.weather[0].description;
+        card.querySelector("#description").innerHTML = description[0].toUpperCase() + description.substring(1);
+        card.querySelector("#wind").innerHTML = weatherOfDat.wind.speed.toFixed(1) + " м/с, " + directionByDergees(weatherOfDat.wind.deg);
+        card.querySelector("#water").innerHTML = weatherOfDat.main.humidity + "%";
+        intexTomorrow += 4;
+    }
+}
+//
+function directionByDergees(dergees) {
+    if (dergees < 22.5) return "С";
+    if (dergees < 67.5) return "СВ";
+    if (dergees < 112.5) return "В";
+    if (dergees < 157.5) return "ЮВ";
+    if (dergees < 202.5) return "Ю";
+    if (dergees < 247.5) return "ЮЗ";
+    if (dergees < 292.5) return "З";
+    if (dergees < 337.5) return "СЗ";
+    return "С";
+}
+
+const templateCard = document.querySelector("#CardOfWeather");
+const templateMainCard = document.querySelector("#main_card");
+const container = document.querySelector("#main_container");
+var date = new Date();
+weather = await getWeatherRequest(latitude, longitude, KeyAPI);
+var intexTomorrow = findIndexNextDay(weather);
+const iconCard = document.querySelector("icon_weather");
+navigator.geolocation.getCurrentPosition(success, error, options);
+fillCards(4);
