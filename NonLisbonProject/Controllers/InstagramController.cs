59f95c65ed94ab5dc31e7703abeab545576221a8
Ilya Kornoushkin <ilyakornoushkin@mail.ru>
2022-03-17 01:16:20 +0400

    Добавил класс для запуска скрипта на питоне

--- NonLisbonProject/Controllers/InstagramController.cs
+++ NonLisbonProject/Controllers/InstagramController.cs
@@ -1,55 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NonLisbonProject.Services;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
+
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace NonLisbonProject.Controllers
 {
-    [Route("api/sasha")]
+    [Route("api/[controller]")]
     [ApiController]
     public class InstagramController : ControllerBase
     {
-        readonly string pathPython = "fdsfds";
+
         // GET: api/<InstagramController>
         [HttpGet]
-        public ActionResult<List<string>> Get(string filename)
+        public async Task<ActionResult<IEnumerable<string>>> GetAsync(string filename)
         {
-            //запустить питоновский скрипт
+            var links = await InstagramService.GetLinks(filename);
+            Console.WriteLine("Сделал");
 
-            var links = new List<string>();
-            links.Add(filename);
-            links.Add(filename);
-            links.Add(filename);
-            links.Add(filename);
-            //прочитать этот файл и закинуть все в links
+            return Ok(links);
+        }
 
-            //удалить файл, который создал питон файл
+        // GET: api/<InstagramController>
+        [HttpGet("{count}")]
+        public async Task<ActionResult<IEnumerable<string>>> GetCountAsync(int count, string filename)
+        {
+            var links = await InstagramService.GetCountLinks(filename, count);
+            Console.WriteLine("Сделал");
+
             return Ok(links);
         }
 
         //может быть мы будем использовать пост запросы, хз для чего
-
         // POST api/<InstagramController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-/*
-        // PUT api/<InstagramController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-*/
         // DELETE api/<InstagramController>
         [HttpDelete()]
         public void Delete()
         {
         }
     }
+
 }
---
+++ NonLisbonProject/Services/InstagramService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+
+namespace NonLisbonProject.Services
+{
+    public static class InstagramService
+    {
+        const string pathPython1 = @"C:\Users\IK\miniconda3\python.exe";
+        const string pathPython2 = @"";
+
+        public static async Task<List<string>> GetLinks(string filename)
+        {
+            //запустить питоновский скрипт
+            Run_Py_Script(filename);
+
+            var links = new List<string>();
+            string path = filename + ".txt";
+            
+            if (File.Exists(path))
+            {
+                // асинхронное чтение
+
+                var fileText = await File.ReadAllLinesAsync(path, Encoding.UTF8);
+                //прочитать этот файл и закинуть все в links
+                links = fileText.ToList();
+                
+            }
+            
+            //удалить файл, который создал питон файл
+            File.Delete(path);
+            return links;
+        }
+
+        public static async Task<List<string>> GetCountLinks(string filename, int count)
+        {
+            //запустить питоновский скрипт
+            Run_Py_Script(filename);
+
+            var links = new List<string>();
+            string path = filename + ".txt";
+
+            if (File.Exists(path))
+            {
+                // асинхронное чтение
+                StreamReader sr = new StreamReader(path);
+                int counter = 0;
+
+                while (!sr.EndOfStream && counter++ < count)
+                {
+                    links.Add(sr.ReadLine());
+                    
+                }
+                sr.Close();
+            }
+
+            //удалить файл, который создал питон файл
+            File.Delete(path);
+            return links;
+        }
+
+        private static void Run_Py_Script(string filename)
+        {
+            // 1) Create Process Info
+            var psi = new ProcessStartInfo();
+            //Путь к питону
+            psi.FileName = pathPython1;
+
+            // 2) Provide script and arguments
+            //Путь к скрипту, только файлы py, русские символы в сылке не читает
+            var script = @"InstagramRequest.py";
+
+            psi.Arguments = $"\"{script}\" \"{filename}\" ";
+
+            // 3) Process configuration
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
+
+            Process.Start(psi);
+        }
+    }
+
+    
+}
